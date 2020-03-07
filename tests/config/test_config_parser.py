422c3eab633d5904516f3125eb8959f02b4c30ec
Sarthak Gupta <sarthakgupta072@gmail.com>
2020-03-07 17:08:33 +0530

    test init

--- tests/config/test_config_parser.py
+++ tests/config/test_config_parser.py
@@ -4,7 +4,20 @@ Tests for config_parser.py
 
 import pytest
 
-from archetype_flask_connexion.config.config_parser import YAMLConfigParser
 
-def test_sample():
-    assert True
+from archetype_flask_connexion.config.config_parser import YAMLConfigParser
+import yaml
+from addict import Dict
+"""
+Test for update_from_yaml function.
+
+It should accept a config_path and config_var (environment variables)
+"""
+def test_update_from_yaml(mocker):
+    instance = YAMLConfigParser()
+    config_path = ["tests/config/sample1.yaml"]
+    config_var = []
+    mocker.patch('archetype_flask_connexion.config.config_parser.YAMLConfigParser.update')
+    res = instance.update_from_yaml(config_path, config_var)
+    assert isinstance(res, str)
+
