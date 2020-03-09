8b93d800fd851b5ddfea5dbc839f01d5e0266983
Sarthak Gupta <sarthakgupta072@gmail.com>
2020-03-09 15:00:14 +0530

    add get_conf test

--- tests/config/test_config_parser.py
+++ tests/config/test_config_parser.py
@@ -4,15 +4,14 @@ Tests for config_parser.py
 
 import pytest
 from archetype_flask_connexion.config.config_parser import YAMLConfigParser
-
-"""
-Test that update_from_yaml return a string object
-"""
+from archetype_flask_connexion.config.config_parser import get_conf
 
 
+# Test that update_from_yaml return a string object
 def test_update_from_yaml_should_returns_str(monkeypatch):
 
     instance = YAMLConfigParser()
+    print(instance)
     config_path = ["tests/config/sample1.yaml"]
     config_var = []
 
@@ -32,3 +31,15 @@ def test_update_from_yaml_should_return_FileNotFoundError():
         config_path = ["tests/config/sample2.yaml"]
         config_var = []
         instance.update_from_yaml(config_path, config_var)
+
+
+# get_conf should return correct key value on call
+def test_get_conf_type():
+
+    myDict = {
+        "config1": "val1",
+        "config2": "val1"
+    }
+    arg1 = "config1"
+    res1 = get_conf(myDict, arg1)
+    assert res1 == "val1"
