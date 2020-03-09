8edfa4b0ab51c19870806409c91277314698bc21
Sarthak Gupta <sarthakgupta072@gmail.com>
2020-03-09 15:29:24 +0530

    Add more test

--- tests/config/test_config_parser.py
+++ tests/config/test_config_parser.py
@@ -5,9 +5,14 @@ Tests for config_parser.py
 import pytest
 from archetype_flask_connexion.config.config_parser import YAMLConfigParser
 from archetype_flask_connexion.config.config_parser import get_conf
+from archetype_flask_connexion.config.config_parser import get_conf_type
 
 
-# Test that update_from_yaml return a string object
+"""
+Test that update_from_yaml return a string object
+"""
+
+
 def test_update_from_yaml_should_returns_str(monkeypatch):
 
     instance = YAMLConfigParser()
@@ -23,7 +28,12 @@ def test_update_from_yaml_should_returns_str(monkeypatch):
     assert isinstance(res, str)
 
 
-# update_from_yaml should return FileNotFoundError when file path is incorrect
+"""
+update_from_yaml should return FileNotFoundError
+when file path is incorrect
+"""
+
+
 def test_update_from_yaml_should_return_FileNotFoundError():
 
     with pytest.raises(FileNotFoundError):
@@ -33,13 +43,53 @@ def test_update_from_yaml_should_return_FileNotFoundError():
         instance.update_from_yaml(config_path, config_var)
 
 
-# get_conf should return correct key value on call
-def test_get_conf_type():
+"""
+get_conf should return correct key value on call
+"""
+
+
+def test_get_conf():
 
     myDict = {
         "config1": "val1",
-        "config2": "val1"
+        "config2": "val2"
     }
     arg1 = "config1"
+    arg2 = "config2"
     res1 = get_conf(myDict, arg1)
+    res2 = get_conf(myDict, arg2)
     assert res1 == "val1"
+    assert res2 == "val2"
+
+
+"""
+get_conf should raise KeyError when an illegal
+value is provided for 'args' and touchy is false
+"""
+
+
+def test_get_conf_type_key_error():
+
+    with pytest.raises(KeyError):
+        myDict = {
+            "config1": "val1",
+            "config2": "val2"
+        }
+        arg1 = "config3"
+        get_conf_type(myDict, arg1, touchy=False)
+
+
+"""
+get_conf_type should return SystemError
+when touchy is false and any exception is raised
+"""
+
+
+def test_get_conf_type_system_error():
+    myDict = {
+        "config1": "val1",
+        "config2": "val2"
+        }
+    with pytest.raises(SystemExit):
+        arg1 = "config3"
+        get_conf_type(myDict, arg1)
