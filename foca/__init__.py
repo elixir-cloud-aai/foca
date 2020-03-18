271c147b9fca607a058c827098862a062611f916
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-03-18 21:50:02 +0100

    clean out setup script

--- foca/__init__.py
+++
@@ -1,4 +0,0 @@
-from foca.config.config_parser import YAMLConfigParser
-from foca.config.config_parser import get_conf
-from foca.config.config_parser import get_conf_type
-from foca.config.log_config import configure_logging
