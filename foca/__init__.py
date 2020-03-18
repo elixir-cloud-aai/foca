f0ee52074b7606df70f07464f9c2c74277cc4ddd
Sarthak Gupta <sarthakgupta072@gmail.com>
2020-03-19 00:15:16 +0530

    minor change

---
+++ foca/__init__.py
@@ -0,0 +1,4 @@
+from foca.config.config_parser import YAMLConfigParser
+from foca.config.config_parser import get_conf
+from foca.config.config_parser import get_conf_type
+from foca.config.log_config import configure_logging
