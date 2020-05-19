3aec31985704948112190667dde29293ec04ad6b
kushagra_9 <nagorikushagra9@gmail.com>
2020-05-19 19:34:49 +0530

    added init file and basic tests for auth

--- tests/security/test_auth.py
+++ tests/security/test_auth.py
@@ -1,13 +1,44 @@
-import sys
-print(sys.path)
-import pytest
-from foca.config.config_parser import YAMLConfigParser
-from foca.security.auth import parse_jwt_from_header
-import unittest
-from unittest.mock import patch
+"""
+Tests for auth.py
+"""
 
-@patch('foca.security.auth.request.headers.get')
-def test_parse_jwt_from_header(mock_get):
-    mock_get.return_value = 'Auth Bearer'
-    ret = parse_jwt_from_header()
-    assert ret=='Auth'
+import pytest
+from connexion import request
+from connexion.exceptions import Unauthorized
+from unittest.mock import Mock
+from foca.security.auth import param_pass
+
+"""
+Test for checking authorization requirement
+"""
+
+def test_not_authorized():
+    @param_pass(authorization_required=False)
+    def mock_func():
+        p = locals()
+        return len(p)
+    assert mock_func() == 0
+
+"""
+Test for the presence of validation methods
+"""
+
+def test_no_validation_methods_present():
+    with pytest.raises(Unauthorized):
+        @param_pass(validation_methods = [])
+        def mock_func():
+            p = locals()
+            return len(p)
+        assert mock_func() == 0
+
+"""
+Test for ensuring the validity of validation checks argument
+"""
+
+def test_valid_validation_methods_args():
+    with pytest.raises(Unauthorized):
+        @param_pass(validation_checks = "")
+        def mock_func():
+            p = locals()
+            return len(p)
+        assert mock_func() == 0
