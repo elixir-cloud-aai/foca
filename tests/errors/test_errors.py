7a9118e7352183b7a87d4dc94bb5da1d4f69f884
kushagra_9 <nagorikushagra9@gmail.com>
2020-05-23 15:55:22 +0530

    added basic errors module and unit tests

---
+++ tests/errors/test_errors.py
@@ -0,0 +1,63 @@
+from foca.errors.errors import (
+    __handle_internal_server_error,
+    __handle_task_not_found,
+    __handle_forbidden,
+    __handle_unauthorized,
+    handle_bad_request
+)
+import json
+
+
+def test_internal_server_error():
+    error = __handle_internal_server_error(Exception)
+    assert error.status == '500 INTERNAL SERVER ERROR'
+    assert error.mimetype == "application/problem+json"
+    response = json.loads(error.data.decode('utf-8'))
+    assert response == {
+        "msg": "An unexpected error occurred.",
+        "status_code": "500"
+        }
+
+
+def test_task_not_found():
+    error = __handle_task_not_found(Exception)
+    assert error.status == '404 NOT FOUND'
+    assert error.mimetype == "application/problem+json"
+    response = json.loads(error.data.decode('utf-8'))
+    assert response == {
+        "msg": "The requested task was not found.",
+        "status_code": "404"
+        }
+
+
+def test_forbidden():
+    error = __handle_forbidden(Exception)
+    assert error.status == '403 FORBIDDEN'
+    assert error.mimetype == "application/problem+json"
+    response = json.loads(error.data.decode('utf-8'))
+    assert response == {
+        "msg": "The requester is not authorized to perform this action.",
+        "status_code": "403"
+        }
+
+
+def test_unauthorized():
+    error = __handle_unauthorized(Exception)
+    assert error.status == '401 UNAUTHORIZED'
+    assert error.mimetype == "application/problem+json"
+    response = json.loads(error.data.decode('utf-8'))
+    assert response == {
+        "msg": "The request is unauthorized.",
+        "status_code": "401"
+        }
+
+
+def test_bad_request():
+    error = handle_bad_request(Exception)
+    assert error.status == '400 BAD REQUEST'
+    assert error.mimetype == "application/problem+json"
+    response = json.loads(error.data.decode('utf-8'))
+    assert response == {
+        "msg": "The request is malformed.",
+        "status_code": "400"
+        }
