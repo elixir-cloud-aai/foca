8ce8d7c862bb75cfe2f9aebdeafe3df013495ed5
Sarthak Gupta <sarthakgupta072@gmail.com>
2020-05-25 23:18:36 +0530

    feat: register MongoDB (#37)
    Co-authored-by: Alex Kanitz <alexander.kanitz@unibas.ch>

--- tests/config/test_log_config.py
+++ tests/config/test_log_config.py
@@ -10,56 +10,45 @@ import os
 
 
 CONFIG_VAR_OK = os.path.abspath(
-        os.path.join(
-            os.path.dirname(
-                os.path.realpath(__file__)
-            ),
-            'log_config.yaml'
-        )
+    os.path.join(
+        os.path.dirname(
+            os.path.realpath(__file__)
+        ),
+        'log_config.yaml'
     )
+)
 
 CONFIG_PATH_INVALID = os.path.abspath(
-        os.path.join(
-            os.path.dirname(
-                os.path.realpath(__file__)
-            ),
-            'log_config1.yaml'
-        )
+    os.path.join(
+        os.path.dirname(
+            os.path.realpath(__file__)
+        ),
+        'log_config1.yaml'
     )
-
-"""
-dictConfig is called once if config_var is undefined
-"""
+)
 
 
 @patch('foca.config.log_config.dictConfig')
 def test_configure_logging_config_var_undefined(test_patch):
+    """dictConfig is called once if config_var is undefined"""
     configure_logging("")
     assert test_patch.called is True
 
 
-"""
-dictConfig is not called if config_path does not have any file
-"""
-
-
 @patch('foca.config.log_config.dictConfig')
 def test_configure_logging_config_var_not_defined__file_not_found(test_patch):
+    """dictConfig is not called if config_path does not have any file"""
     # Assuming it will raise FileNotFoundError
     YAMLConfigParser.update_from_yaml = MagicMock(
-                                                side_effect=FileNotFoundError
-                                                )
+        side_effect=FileNotFoundError
+    )
     configure_logging(default_path=str(CONFIG_PATH_INVALID))
     assert test_patch.called is False
 
 
-"""
-dictConfig is called once if config_var is defined
-"""
-
-
 @patch('foca.config.log_config.dictConfig')
 def test_configure_logging_config_var_defined(test_patch):
+    """dictConfig is called once if config_var is defined"""
     os.environ['TEST_LOG_CONFIG'] = CONFIG_VAR_OK
     YAMLConfigParser.update_from_yaml = MagicMock()
     configure_logging(config_var="TEST_LOG_CONFIG")
