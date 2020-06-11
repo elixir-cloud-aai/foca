2b84de9cc27711bad0a47137969a8c0bede68b95
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-06-11 17:14:24 +0200

    playing around

--- tests/factories/test_celery_app.py
+++ tests/factories/test_celery_app.py
@@ -1,16 +1,19 @@
-from foca.factories.celery_app import create_celery_app
+"""Unit tests for Celery app factory module."""
+
 from celery import Celery
 from flask import Flask
+import pytest
 
+from foca.factories.celery_app import create_celery_app
 
 MOCK_HOST = "mock_host"
 MOCK_PORT = "mock_port"
 DICT_CONF_VALS = {
     'broker_host': "broker_host",
-    'broker_port': "broker_port",
+    'broker_port': 5762,
     'result_backend': "result_backend",
     'include': "include",
-    'message_maxsize': "message_maxsize"
+    'message_maxsize': 10000
 }
 
 
@@ -41,10 +44,66 @@ def test_create_celery_app(monkeypatch):
     )
 
     demo_app = Flask(__name__)
-    demo_app.config['BROKER_URL'] = 'sqla+sqlite:///celerydb.sqlite'
-    demo_app.config['CELERY_RESULT_BACKEND'] = 'db+sqlite:///results.sqlite'
+    demo_app.config['BROKER_URL'] = "broker_url = 'amqp://myuser:mypassword@localhost:5672/myvhost"
     demo_app.config['celery'] = 'celery'
-    res_app = create_celery_app(demo_app)
-    celery_app_ret_type = type(Celery(app=__name__))
+    celery = create_celery_app(demo_app)
 
-    assert type(res_app) == celery_app_ret_type
+    #@celery.task()
+    #def add_together(a, b):
+    #    return a + b
+
+    #result = add_together.delay(23, 42)
+
+    assert type(celery) == type(Celery(app=__name__))
+
+
+class eager_conf:
+    """Configuration for testing Celery tasks for Celery >= 4.0."""
+
+    CELERY_TASK_ALWAYS_EAGER = True
+    CELERY_TASK_EAGER_PROPAGATES = True
+    CELERY_RESULT_BACKEND = "cache"
+    CELERY_CACHE_BACKEND = "memory"
+
+@pytest.fixture(scope='session')
+def celery_config():
+    return {
+        'broker_url': 'amqp://',
+        'result_backend': 'redis://'
+    }
+
+def test_appctx_task(monkeypatch, celery_worker):
+    """Test execution of Celery task with application context."""
+    app = Flask("myapp")
+    app.config.from_object(eager_conf)
+
+    def mock_get_conf(*args, **kwargs):
+        return DICT_CONF_VALS[list(args)[2]]
+
+    monkeypatch.setattr(
+        'foca.factories.celery_app.get_conf',
+        mock_get_conf
+    )
+
+    monkeypatch.setattr(
+        'foca.factories.celery_app.get_conf_type',
+        lambda *args, **kwargs: DICT_CONF_VALS['include']
+    )
+
+    monkeypatch.setattr(
+        'foca.factories.celery_app.get_conf_type',
+        lambda *args, **kwargs: DICT_CONF_VALS['include']
+    )
+
+    # Set the current Celery application
+    c = Celery('mycurrent')
+    c.set_current()
+
+    celery = create_celery_app(app)
+
+    @celery.task
+    def appctx():
+        return current_app.name
+
+    r = appctx.delay()
+    assert r.result == "myapp"
