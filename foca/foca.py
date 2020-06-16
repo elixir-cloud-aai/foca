dd5d469470d52714b9219c89f26b7fc3bfbba39c
Kostas Liosis <liosis.kc@gmail.com>
2020-06-16 20:44:24 +0300

    feat(api): changes in foca entry point
    foca() now accepts an absolute path to a YAML configuration file which then passes to YAMLConfigParser. The input file gets validated and Config class instance gets returned, in order for the app to be initialized.
    
    (#42)

--- foca/foca.py
+++ foca/foca.py
@@ -1,77 +1,39 @@
 """ Definition of an entry-point function for setting up and initializing a
 FOCA based microservice."""
 
+from connexion import App
 from foca.api.register_openapi import register_openapi
+from foca.config.config_parser import YAMLConfigParser
 from foca.config.log_config import configure_logging
 from foca.database.register_mongodb import register_mongodb
 from foca.errors.errors import register_error_handlers
 from foca.factories.connexion_app import create_connexion_app
 from foca.factories.celery_app import create_celery_app
 from foca.security.cors import enable_cors
-from foca.config.config_handler import Config
-from connexion import App
-from typing import Mapping, Any
 
 
-def foca(config: Mapping[str, Any]
+def foca(config: str
          ) -> App:
     """ Initialization of a FOCA based microservice.
 
     Args:
-        config: A Dict object, parsed from a proper YAML configuration file,
-                containing the required FOCA-specific configuration parameters
-                (under the reserved `foca` key), along with any
-                serivce-specific parameters. It should follow the format shown
-                below:
-
-                {
-                    foca:
-                        db:
-                            # Any database parameters
-                        api:
-                            # Any OpenAPI specifications
-                        errors:
-                            # Any custom errors
-                        celery:
-                            # Any Celery parameters for background jobs
-                        log:
-                            # Any log parameters
-                        security:
-                            # Any protected endpoints/security parameters
-                        server:
-                            # Any general Flask/Connexion/Gunicorn parameters
-                            # (e.g., host, port...)
-
-                    # Any parameter below this point is service-specifc and
-                    will be added to `app.config` without validation
-
-                    service_specific_section_1:
-                        param_1: my_param_1
-                        param_2: my_param_2
-                    service_specific_section_2:
-                        param_1: my_param_1
-                        param_2: my_param_2
-                }
+        config: An absolute path to a YAML application configuration file that
+                follows the proper format. To properly structure the YAML file,
+                see :py:class:`foca.models.config.Config()`
 
     Returns:
         A Connexion app instance
 
     Example:
 
-        from foca.foca import foca
-        import yaml
-
-        with open('/path/to/config.yaml') as config_file:
-            config = yaml.safe_load(config_file)
-
-        app = foca(**config)
+        app = foca('/path/to/config.yaml')
     """
 
     # Configure logger
     configure_logging(config_var='FOCA_CONFIG_LOG')
 
     # Create a Config class instance for parameters validation
-    conf = Config(config)
+    conf = YAMLConfigParser(config)
 
     # Create Connexion app
     connexion_app = create_connexion_app(conf.dict())
