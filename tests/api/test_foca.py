c73de2288babce9503bf955673287ef8f0ef1ea7
Kostas Liosis <liosis.kc@gmail.com>
2020-06-18 00:04:23 +0300

    feat(api): flake8 warning fixes
    Minor linter issues
    
    ((#42)

--- tests/api/test_foca.py
+++ tests/api/test_foca.py
@@ -13,14 +13,17 @@ DIR = pathlib.Path(__file__).parent.parent / "test_files"
 EMPTY_CONF = str(DIR / "empty_conf.yaml")
 INVALID_CONF = str(DIR / "invalid_conf.yaml")
 
+
 def test_foca_output():
     """Ensure foca() returns a Connexion app instance"""
     assert isinstance(foca(EMPTY_CONF), App)
+
 
 def test_foca_invalid_structure():
     """Test foca(); invalid configuration file structure"""
     with pytest.raises(ValidationError):
         foca(INVALID_CONF)
+
 
 def test_foca_invalid_YAML():
     """Test foca(); invalid configuration file format"""
