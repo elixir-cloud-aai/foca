aff81d0ea23127ca65fbd51efebd7c4bd126c7b6
kushagra_9 <nagorikushagra9@gmail.com>
2020-06-22 09:13:58 +0530

    playing around with petstore

---
+++ examples/basic-app/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from foca.foca import foca
+
+PETS = {}
+
+
+def listPets():
+    return {"pets": [pet for pet in PETS.values()]}
+
+
+def createPets(pet):
+    PETS[pet] = 1
+    return {"pets": [pet for pet in PETS.values()]}
+
+
+def showPetById(id):
+    if id in PETS:
+        return PETS[id]
+    else:
+        return {"No pets found"}
+
+
+if __name__ == '__main__':
+    app = foca("conf_valid.yaml")
+    app.run(port=8080)
