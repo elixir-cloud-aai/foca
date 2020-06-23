1612f7e8c37824254a13bbd238a01f4c2c8f9698
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-06-24 00:17:35 +0200

    feat(db): support for additional MongoDB features (#58)
    Co-authored-by: Kostas Liosis <liosis.kc@gmail.com>

--- tests/config/test_config_parser.py
+++ tests/config/test_config_parser.py
@@ -13,21 +13,20 @@ from foca.models.config import Config
 TEST_FILE = "tests/test_files/conf_valid.yaml"
 TEST_FILE_INVALID = "tests/test_files/conf_invalid_log_level.yaml"
 TEST_DICT = {}
-TEST_CONFIG_INSTACE = Config()
+TEST_CONFIG_INSTANCE = Config()
 
 
 def test_config_parser_valid_config_file():
     """Test valid YAML parsing"""
     conf = ConfigParser(TEST_FILE)
     assert type(conf.config.dict()) == type(TEST_DICT)
-    assert type(conf.config) == type(TEST_CONFIG_INSTACE)
+    assert isinstance(conf.config, type(TEST_CONFIG_INSTANCE))
 
 
 def test_config_parser_invalid_config_file():
     """Test valid YAML parsing"""
     with pytest.raises(ValidationError):
-        conf = ConfigParser(TEST_FILE_INVALID)
-        assert conf is not None
+        ConfigParser(TEST_FILE_INVALID)
 
 
 def test_config_parser_invalid_file_path():
