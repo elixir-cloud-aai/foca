48681272eb71273f60236dc6c03c488af1b0f00b
Konstantinos Christos Liosis <liosis.kc@gmail.com>
2020-06-24 01:46:11 +0300

    feat: add main module (#48)
    Co-authored-by: Alex Kanitz <alexander.kanitz@unibas.ch>

---
+++ foca/foca.py
@@ -0,0 +1,73 @@
+""" Definition of an entry-point function for setting up and initializing a
+FOCA based microservice."""
+
+import logging
+from typing import Optional
+
+from connexion import App
+
+from foca.api.register_openapi import register_openapi
+from foca.config.config_parser import ConfigParser
+from foca.database.register_mongodb import register_mongodb
+from foca.errors.errors import register_error_handlers
+from foca.factories.connexion_app import create_connexion_app
+from foca.factories.celery_app import create_celery_app
+from foca.security.cors import enable_cors
+
+# Get logger instance
+logger = logging.getLogger(__name__)
+
+
+def foca(config: Optional[str] = None) -> App:
+    """ Initialize a FOCA-based microservice.
+
+    Args:
+        config: Path to a YAML application configuration file. For required
+            YAML file structure, see :py:class:`foca.models.config.Config()`.
+
+    Returns:
+        Connexion app instance.
+    """
+    # Create a Config class instance for parameters validation
+    conf = ConfigParser(config).config
+    logger.info(f"Configuration file '{config}' parsed.")
+
+    # Create Connexion app
+    cnx_app = create_connexion_app(conf)
+    logger.info(f"Connexion app created.")
+
+    # Register error handlers
+    cnx_app = register_error_handlers(cnx_app)
+    logger.info(f"Error handlers registered.")
+
+    # Register MongoDB
+    if conf.db:
+        cnx_app.app.config['FOCA'].db = register_mongodb(
+            app=cnx_app.app,
+            conf=conf.db,
+        )
+        logger.info(f"Database registered.")
+    else:
+        logger.info(f"No database support configured.")
+
+    # Create Celery app
+    if conf.jobs:
+        create_celery_app(cnx_app.app)
+        logger.info(f"Support for background tasks set up.")
+    else:
+        logger.info(f"No support for background tasks configured.")
+
+    # Register OpenAPI specs
+    if conf.api.specs:
+        cnx_app = register_openapi(
+            app=cnx_app,
+            specs=conf.api.specs,
+        )
+    else:
+        logger.info(f"No OpenAPI specifications provided.")
+
+    # Enable cross-origin resource sharing
+    enable_cors(cnx_app.app)
+    logger.info(f"CORS enabled.")
+
+    return cnx_app
---
+++ tests/test_foca.py
@@ -0,0 +1,111 @@
+"""Tests for `foca.py` module."""
+
+import pathlib
+import pytest
+import shutil
+import os
+
+from connexion import App
+from connexion.exceptions import InvalidSpecification
+from pydantic import ValidationError
+from pymongo.collection import Collection
+from pymongo.database import Database
+from yaml import safe_load, safe_dump, YAMLError
+
+from foca.foca import foca
+
+DIR = pathlib.Path(__file__).parent / "test_files"
+PATH_SPECS_2_YAML = str(DIR / "openapi_2_petstore.yaml")
+PATH_SPECS_2_YAML_MODIFIED = str(DIR / "openapi_2_petstore.modified.yaml")
+PATH_SPECS_INVALID_OPENAPI = str(DIR / "invalid_openapi_2.yaml")
+EMPTY_CONF = str(DIR / "empty_conf.yaml")
+INVALID_CONF = str(DIR / "invalid_conf.yaml")
+VALID_CONF = str(DIR / "conf_valid.yaml")
+VALID_DB_CONF = str(DIR / "conf_db.yaml")
+INVALID_DB_CONF = str(DIR / "invalid_conf_db.yaml")
+INVALID_LOG_CONF = str(DIR / "conf_invalid_log_level.yaml")
+JOBS_CONF = str(DIR / "conf_jobs.yaml")
+INVALID_JOBS_CONF = str(DIR / "conf_invalid_jobs.yaml")
+API_CONF = str(DIR / "conf_api.yaml")
+
+
+def create_temporary_copy(path, CONF):
+    """Create a copy of a configuration YAML file"""
+    temp_path = os.path.join(DIR, 'temp_test.yaml')
+    shutil.copy2(path, temp_path)
+    with open(temp_path, 'r+') as conf_file:
+        conf = safe_load(conf_file)
+        conf["api"]["specs"][0]["path"] = CONF
+        conf["api"]["specs"][0]["path_out"] = PATH_SPECS_2_YAML_MODIFIED
+        conf_file.seek(0)
+        safe_dump(conf, conf_file)
+        conf_file.truncate()
+    return temp_path
+
+
+def test_foca_output_defaults():
+    """Ensure foca() returns a Connexion app instance; defaults only."""
+    app = foca()
+    assert isinstance(app, App)
+
+
+def test_foca_empty_conf():
+    """Ensure foca() require non-empty configuration parameters (if a field is
+    present)"""
+    with pytest.raises(ValidationError):
+        foca(EMPTY_CONF)
+
+
+def test_foca_invalid_structure():
+    """Test foca(); invalid configuration file structure."""
+    with pytest.raises(YAMLError):
+        foca(INVALID_CONF)
+
+
+def test_foca_invalid_log():
+    """Test foca(); invalid log field"""
+    with pytest.raises(ValidationError):
+        foca(INVALID_LOG_CONF)
+
+
+def test_foca_jobs():
+    """Ensure foca() returns a Connexion app instance; valid jobs field"""
+    app = foca(JOBS_CONF)
+    assert isinstance(app, App)
+
+
+def test_foca_invalid_jobs():
+    """Test foca(); invalid jobs field"""
+    with pytest.raises(ValidationError):
+        foca(INVALID_JOBS_CONF)
+
+
+def test_foca_api():
+    """Ensure foca() returns a Connexion app instance; valid api field"""
+    temp_file = create_temporary_copy(API_CONF, PATH_SPECS_2_YAML)
+    app = foca(temp_file)
+    assert isinstance(app, App)
+    os.remove(temp_file)
+
+
+def test_foca_invalid_api():
+    """Test foca(); invalid api field (Invalid Connexion spec)"""
+    temp_file = create_temporary_copy(API_CONF, PATH_SPECS_INVALID_OPENAPI)
+    with pytest.raises(InvalidSpecification):
+        foca(temp_file)
+
+
+def test_foca_db():
+    """Ensure foca() returns a Connexion app instance; valid db field"""
+    app = foca(VALID_DB_CONF)
+    my_db = app.app.config['FOCA'].db.dbs['my-db']
+    my_coll = my_db.collections['my-col-1']
+    assert isinstance(my_db.client, Database)
+    assert isinstance(my_coll.client, Collection)
+    assert isinstance(app, App)
+
+
+def test_foca_invalid_db():
+    """Test foca(); invalid db field"""
+    with pytest.raises(ValidationError):
+        foca(INVALID_DB_CONF)
