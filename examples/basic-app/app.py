1f538062a761e5057cd4d978a6b3e2edd1391175
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-06-24 19:31:36 +0200

    fix some Dockerfile issues

--- examples/basic-app/app.py
+++ examples/basic-app/app.py
@@ -7,26 +7,8 @@ from foca.foca import foca
 
 logger = logging.getLogger(__name__)
 
-PETS = {}
-
-def listPets():
-    return {"pets": [pet for pet in PETS.values()]}
-
-
-def createPets(pet):
-    PETS[pet] = 1
-    return {"pets": [pet for pet in PETS.values()]}
-
-
-def showPetById(id):
-    if id in PETS:
-        return PETS[id]
-    else:
-        return {"No pets found"}
-
-
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     config = str(Path(__file__).resolve().parent / "config.yaml")
     app = foca(config)
     app.run(port=8080)
---
+++ examples/basic-app/controllers/__init__.py
@@ -0,0 +1,16 @@
+PETS = {}
+
+def listPets():
+    return {"pets": [pet for pet in PETS.values()]}
+
+
+def createPets(pet):
+    PETS[pet] = 1
+    return {"pets": [pet for pet in PETS.values()]}
+
+
+def showPetById(id):
+    if id in PETS:
+        return PETS[id]
+    else:
+        return {"No pets found"}
