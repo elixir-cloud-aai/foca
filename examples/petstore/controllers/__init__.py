bea5f81fb12ff1266087ec10a24f5692969bdf14
kushagra_9 <nagorikushagra9@gmail.com>
2020-06-27 23:18:11 +0530

    added and checked all the endpoints

--- examples/petstore/controllers/__init__.py
+++ examples/petstore/controllers/__init__.py
@@ -6,33 +6,74 @@ db = MongoClient(host='mongodb', port=27017)
 db_collection = db.petstore.pets
 
 
-def findPets(limit=None, tag=None):
-    if limit is None:
-        records = db_collection.find({}).sort([('$natural', -1)])
-    else:
-        records = db_collection.find({}).sort([('$natural', -1)]).limit(limit)
+def findPets(limit=None, tags=None):
+    try:
+        filter_dict = {} if tags is None else {'tag': {'$in': tags}}
+        if limit is None:
+            records = db_collection.find(filter_dict).sort([('$natural', -1)])
+        else:
+            records = db_collection.find(filter_dict).sort([('$natural', -1)]).limit(limit)
 
-    response_arr = []
-    for doc in records:
-        record_result = {
-                "name": doc['name'],
-                "tag": doc['tag'],
-                "id": int(str(doc['_id']), 16)
-            }
-        response_arr.append(record_result)
+        response_arr = []
+        for doc in records:
+            record_result = {
+                    "name": doc['name'],
+                    "tag": doc['tag'],
+                    "id": doc['pet_id']
+                }
+            response_arr.append(record_result)
 
-    return json.dumps(response_arr)
-
+        return json.dumps(response_arr)
+    except:
+        return json.dumps({
+            'code': 0,
+            'message': 'Cannot view pets. Something went wrong.'
+            })
 
 def addPet(pet):
-    pet_dec = pet
-    db_collection.insert({"name": pet_dec['name'], "tag": pet_dec['tag']})
-    return findPets(limit=1)
+    try:
+        ctr = db_collection.find({}).sort([('$natural', -1)])
+        counter = 0
+        if ctr.count() == 0:
+            counter = 0
+        else:
+            counter = ctr[0].get('pet_id') + 1
+        db_collection.insert({
+            "pet_id": counter,
+            "name": pet['name'],
+            "tag": pet['tag']
+            })
+        return findPets(limit=1)
+    except:
+        return json.dumps({
+            'code': 0,
+            'message': 'Cannot add pet, something went wrong.'
+            })
 
 
-def findpetbyid():
-    return {3}
+def findPetById(id):
+    try:
+        pet_record = db_collection.find({"pet_id": id})[0]
+        if pet_record is None:
+            return json.dumps({'code': 0, 'message': 'Pet does not exists.'})
+        return json.dumps({
+            "name": pet_record['name'],
+            "tag": pet_record['tag'],
+            "id": pet_record['pet_id']
+            })
+    except:
+        return json.dumps({
+            'code': 0,
+            'message': 'Pet does not exists.'
+            })
 
 
-def deletePet():
-    return {4}
+def deletePet(id):
+    try:
+        db_collection.remove({"pet_id": id})
+        return findPets()
+    except:
+        return json.dumps({
+            'code': 0,
+            'message': 'Cannot delete flat. Something went wrong.'
+            })
