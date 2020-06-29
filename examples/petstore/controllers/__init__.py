ff28d51e19ebf22e2bf29c4331517e686e469d9e
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-06-30 01:50:12 +0200

    use DB from app context; fix endpoints

--- examples/petstore/controllers/__init__.py
+++ examples/petstore/controllers/__init__.py
@@ -1,98 +1,89 @@
-from pymongo import MongoClient
 import logging
-import json
 
+from flask import (current_app, make_response)
+from werkzeug.exceptions import NotFound
 
 logger = logging.getLogger(__name__)
 
-
-db = MongoClient(host='mongodb', port=27017)
-db_collection = db.petstore.pets
+error_response = {
+    'code': 500,
+    'message': 'Something went wrong.',
+}
 
 
 def findPets(limit=None, tags=None):
     try:
-        filter_dict = {} if tags is None else {'tag': {'$in': tags}}
-        if limit is None:
-            records = db_collection.find(filter_dict).sort([('$natural', -1)])
-        else:
-            records = db_collection.find(filter_dict).sort(
-                [('$natural', -1)]
-                ).limit(limit)
-
-        response_arr = []
-        for doc in records:
-            record_result = {
-                    "name": doc['name'],
-                    "tag": doc['tag'],
-                    "id": doc['pet_id']
-                }
-            response_arr.append(record_result)
-
-        return json.dumps(response_arr)
-    except Exception as e:
-        logger.error(
-            "Original Error : {error}".format(error=e)
+        db_collection = (
+            current_app.config['FOCA'].db.dbs['petstore']
+            .collections['pets'].client
         )
-        return json.dumps({
-            'code': 0,
-            'message': 'Cannot view pets. Something went wrong.'
-            })
+        filter_dict = {} if tags is None else {'tag': {'$in': tags}}
+        if not limit:
+            limit = 0
+        records = db_collection.find(
+            filter_dict,
+            {'_id': False}
+        ).sort([('$natural', -1)]).limit(limit)
+        return list(records)
+    except Exception as e:
+        logger.error(f"{type(e).__name__}: {e}")
+        return error_response
 
 
 def addPet(pet):
     try:
-        ctr = db_collection.find({}).sort([('$natural', -1)])
+        db_collection = (
+            current_app.config['FOCA'].db.dbs['petstore']
+            .collections['pets'].client
+        )
         counter = 0
-        if ctr.count() == 0:
-            counter = 0
-        else:
-            counter = ctr[0].get('pet_id') + 1
-        db_collection.insert({
-            "pet_id": counter,
+        ctr = db_collection.find({}).sort([('$natural', -1)])
+        if not ctr.count() == 0:
+            counter = ctr[0].get('id') + 1
+        record = {
+            "id": counter,
             "name": pet['name'],
             "tag": pet['tag']
-            })
-        return findPets(limit=1)
+        }
+        db_collection.insert_one(record)
+        del record['_id']
+        return record
     except Exception as e:
-        logger.error(
-            "Original Error : {error}".format(error=e)
-        )
-        return json.dumps({
-            'code': 0,
-            'message': 'Cannot add pet, something went wrong.'
-            })
+        logger.error(f"{type(e).__name__}: {e}")
+        return error_response
 
 
 def findPetById(id):
     try:
-        pet_record = db_collection.find({"pet_id": id})[0]
-        if pet_record is None:
-            return json.dumps({'code': 0, 'message': 'Pet does not exists.'})
-        return json.dumps({
-            "name": pet_record['name'],
-            "tag": pet_record['tag'],
-            "id": pet_record['pet_id']
-            })
-    except Exception as e:
-        logger.error(
-            "Original Error : {error}".format(error=e)
+        db_collection = (
+            current_app.config['FOCA'].db.dbs['petstore']
+            .collections['pets'].client
         )
-        return json.dumps({
-            'code': 0,
-            'message': 'Pet does not exists.'
-            })
+        return db_collection.find_one(
+            {"id": id},
+            {'_id': False},
+        )
+    except Exception as e:
+        logger.error(f"{type(e).__name__}: {e}")
+        return error_response
 
 
 def deletePet(id):
     try:
-        db_collection.remove({"pet_id": id})
-        return findPets()
-    except Exception as e:
-        logger.error(
-            "Original Error : {error}".format(error=e)
+        db_collection = (
+            current_app.config['FOCA'].db.dbs['petstore']
+            .collections['pets'].client
         )
-        return json.dumps({
-            'code': 0,
-            'message': 'Cannot delete flat. Something went wrong.'
-            })
+        record = db_collection.find_one(
+            {"id": id},
+            {'_id': False},
+        )
+        if not record:
+            raise NotFound
+        db_collection.delete_one({"id": id})
+        response = make_response('', 204)
+        response.mimetype = current_app.config['JSONIFY_MIMETYPE']
+        return response
+    except Exception as e:
+        logger.error(f"{type(e).__name__}: {e}")
+        return error_response
