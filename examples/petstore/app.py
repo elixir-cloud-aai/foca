44da77ec31e8c547e0a10cafeea56f3aef1ffc90
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-07-01 20:09:21 +0200

    clean up app.py

--- examples/petstore/app.py
+++ examples/petstore/app.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-import logging
 from pathlib import Path
-import sys
 
 from foca.foca import foca
 
-logger = logging.getLogger(__name__)
-
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     config = str(Path(__file__).resolve().parent / "config.yaml")
     app = foca("config.yaml")
     app.run(port=8080)
-    logger.info("Petstore is running...")
