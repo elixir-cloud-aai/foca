1baff5250932b71f2c1639cae273267d7f4dff8d
Alex Kanitz <alexander.kanitz@unibas.ch>
2021-05-02 16:28:19 +0200

    build: separate development dependencies

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
     install_requires=install_requires,
     include_package_data=True,
     setup_requires=[
-        "setuptools_git == 1.2",
+        "setuptools_git==1.2",
+        "twine==3.1.1"
     ],
 )
