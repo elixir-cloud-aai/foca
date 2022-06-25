dfbbd44c65fa764384afde0529dcada41504c121
Alex Kanitz <alexander.kanitz@alumni.ethz.ch>
2022-06-25 16:06:31 +0200

    test: improve integration tests

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -1,50 +1,158 @@
+"""Integration tests for petstore app."""
+
 import requests
 
+from tests.test_files.models_petstore import (
+    Error,
+    Pet,
+    Pets,
+)
 
-class PetStore:
-    """Petstore class.
-
-    Attributes:
-        url: Petstore API base URL.
-    """
-    def __init__(self):
-        """Class constructor method."""
-        self.url = "http://localhost:80/pets"
-
-    def add_path_to_url(self, id):
-        """Return petstore base URL with appended path"""
-        return "/".join([self.url, str(id)])
-
-
-petstore = PetStore()
+PETSTORE_URL = "http://localhost:80"
+NAME_PET = "karl"
+TAG_PET = "frog"
+EXTRA_PARAM_ARG = "extra"
+BODY_PET_1 = {
+    "name": NAME_PET,
+    "tag": TAG_PET,
+}
+BODY_PET_2 = {
+    "name": NAME_PET,
+    "tag": TAG_PET,
+    "extra_parameter": EXTRA_PARAM_ARG,
+}
+INVALID_ID = "X"
 
 
-def test_post_request():
+def test_add_pet_200():
     """Test `POST /pets` for successfully adding a new pet."""
-    temp_data = {"name": "karl", "tag": "frog"}
-    response = requests.post(petstore.url, json=temp_data)
+    response = requests.post(
+        url=f"{PETSTORE_URL}/pets",
+        json=BODY_PET_1,
+    )
     assert response.status_code == 200
+    response_data = Pet(**response.json())
+    assert isinstance(response_data, Pet)
+    assert response_data.name == NAME_PET
+    assert response_data.tag == TAG_PET
 
 
-def test_get_all_pets_request():
+def test_add_pet_extra_parameter_200():
+    """Test `POST /pets` to ensure that extra parameter is ignored."""
+    response = requests.post(
+        url=f"{PETSTORE_URL}/pets",
+        json=BODY_PET_2,
+    )
+    assert response.status_code == 200
+    response_data = Pet(**response.json())
+    assert isinstance(response_data, Pet)
+    assert response_data.name == NAME_PET
+    assert response_data.tag == TAG_PET
+    assert getattr(response_data, 'extra_parameter', None) is None
+
+
+def test_add_pet_required_arguments_missing_400():
+    """Test `POST /pets` with required arguments missing."""
+    response = requests.post(
+        url=f"{PETSTORE_URL}/pets",
+        json={},
+    )
+    assert response.status_code == 400
+    print(response.json())
+    response_data = Error(**response.json())
+    assert response_data.code == 400
+    assert response_data.message == "The request is malformed."
+
+
+def test_get_all_pets_200():
     """Test `GET /pets` for successfully fetching all pets."""
-    response = requests.get(petstore.url)
+    response = requests.get(
+        url=f"{PETSTORE_URL}/pets",
+    )
     assert response.status_code == 200
+    response_data = Pets(pets=response.json())
+    assert isinstance(response_data, Pets)
 
 
-def test_get_pet_by_id_request():
+def test_get_all_pets_check_record_number():
+    """Test `GET /pets` to ensure that the number of records increased after
+    adding an additional pet.
+    """
+    response = requests.get(
+        url=f"{PETSTORE_URL}/pets",
+    )
+    assert response.status_code == 200
+    records = len(response.json())
+    requests.post(
+        url=f"{PETSTORE_URL}/pets",
+        json=BODY_PET_1,
+    )
+    new_response = requests.get(
+        url=f"{PETSTORE_URL}/pets",
+    )
+    assert new_response.status_code == 200
+    assert len(new_response.json()) == records + 1
+
+
+def test_get_pet_by_id_200():
     """Test for `GET /pets/{id}` for successfully fetching a pet with a given
     id.
     """
-    new_url = petstore.add_path_to_url("0")
-    response = requests.get(new_url)
+    post_response = requests.post(
+        url=f"{PETSTORE_URL}/pets",
+        json=BODY_PET_1,
+    )
+    pet_id = Pet(**post_response.json()).id
+    response = requests.get(
+        url=f"{PETSTORE_URL}/pets/{pet_id}",
+    )
     assert response.status_code == 200
+    response_data = Pet(**response.json())
+    assert isinstance(response_data, Pet)
+    assert response_data.name == NAME_PET
+    assert response_data.tag == TAG_PET
 
 
-def test_delete_request():
+def test_get_pet_by_id_404():
+    """Test for `GET /pets/{id}` for fetching a non-existent pet."""
+    response = requests.get(
+        url=f"{PETSTORE_URL}/pets/{INVALID_ID}",
+    )
+    assert response.status_code == 404
+    response_data = Error(**response.json())
+    assert response_data.code == 404
+    assert response_data.message == "The requested resource wasn't found."
+
+
+def test_delete_pet_204():
     """Test for `DELETE /pets/{id}` for successfully deleting a pet with a
     given id.
     """
-    new_url = petstore.add_path_to_url("0")
-    response = requests.delete(new_url)
+    post_response = requests.post(
+        url=f"{PETSTORE_URL}/pets",
+        json=BODY_PET_1,
+    )
+    pet_id = Pet(**post_response.json()).id
+    get_response_pre = requests.get(
+        url=f"{PETSTORE_URL}/pets/{pet_id}",
+    )
+    assert get_response_pre.status_code == 200
+    response = requests.delete(
+        url=f"{PETSTORE_URL}/pets/{pet_id}",
+    )
     assert response.status_code == 204
+    get_response_post = requests.get(
+        url=f"{PETSTORE_URL}/pets/{pet_id}",
+    )
+    assert get_response_post.status_code == 404
+
+
+def test_delete_pet_404():
+    """Test for `DELETE /pets/{id}` for deleting a non-existent pet."""
+    response = requests.delete(
+        url=f"{PETSTORE_URL}/pets/{INVALID_ID}",
+    )
+    assert response.status_code == 404
+    response_data = Error(**response.json())
+    assert response_data.code == 404
+    assert response_data.message == "The requested resource wasn't found."
